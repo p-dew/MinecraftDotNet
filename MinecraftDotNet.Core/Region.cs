39fe05a0aece80bd755802b1e1db8195bf5c14b1
Vladislav Podporkin <vavus44375@gmail.com>
2019-12-01 03:20:15 +0300

    Added many useless classes.

---
+++ MinecraftDotNet.Core/Region.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using MinecraftDotNet.Core.Math;
+
+namespace MinecraftDotNet.Core
+{
+    public class Region
+    {
+        private Dictionary<Coordinates2, Chunk> _chunks;
+        
+        
+    }
+    
+    
+}
